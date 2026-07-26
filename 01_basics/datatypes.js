4114a3fed0f7935e2db94f01ecc33c03fc30d069
bharatswarajsahu <bharatswarajsahu@gmail.com>
2026-07-26 19:57:33 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as newer version
+//alert("Hello")
+
+console.log(3+3)
+let name = "Bharat";//string literal
+let age = 22;//number literal
+let isApproved = true;//
+let firstName = undefined;//
+let selectedColor = null;//object literal//standalone value
+//symbol =>unique identifier
+//object => key value pair
+console.table([typeof name, typeof age, typeof isApproved, typeof firstName, typeof selectedColor]);
