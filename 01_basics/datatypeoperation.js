0a27ced93bbe9d6a1bf88dc099564824fdfb87aa
bharatswarajsahu <bharatswarajsahu@gmail.com>
2026-07-26 23:10:08 +0530

    datatype operation

---
+++ 01_basics/datatypeoperation.js
@@ -0,0 +1,20 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+let str1 = "Bharat"
+let str2 = "Swaraj"
+let str3 = str1 + " " + str2
+console.log(str3)
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")//exception
+//use perenthesis to avoid exception
+console.log(+true)//1but true is print in simple true
+//true+ = not run
+console.log(+"")
+let num1,num2,num3
+num1=num2=num3=2+3
+let gamecounter =100
+gamecounter++
+console.log(gamecounter)
