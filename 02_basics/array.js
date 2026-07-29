6641c24fbd18352afa810a1fcfe2c434716958f6
bharatswarajsahu <bharatswarajsahu@gmail.com>
2026-07-29 23:31:55 +0530

    array

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,8 +1,8 @@
 const myArr = [1, 2, 3, 4, 5];  
-console.log(myArr[0]) // Output : [1, 2, 3, 4, 5]
+//console.log(myArr[0]) // Output : [1, 2, 3, 4, 5]
 
 const myarr2= new Array(1, 2, 3, 4, 5);
-console.log(myarr2) // Output : [1, 2, 3, 4, 5]
+//console.log(myarr2) // Output : [1, 2, 3, 4, 5]
 
 //array methods
 
@@ -10,7 +10,7 @@ console.log(myarr2) // Output : [1, 2, 3, 4, 5]
 console.log(myArr) // Output : [1, 2, 3, 4, 5, 6]
 
 myarr2.pop(); // Removes the last element from the array
-console.log(myarr2) // Output : [1, 2, 3, 4]*/
+console.log(myarr2) // Output : [1, 2, 3, 4]
 
 myArr.unshift(0); // Adds 0 to the beginning of the array
 console.log(myArr) // Output : [0, 1, 2, 3, 4, 5]
@@ -23,4 +23,16 @@ console.log(myArr.includes(4))//checks availability of element
 const newARR =myArr.join()
 
 console.log(myArr)
-console.log(newARR)
+console.log(newARR)*/
+
+//slice,splice
+
+console.log("A",myArr);
+
+const myn1 =myArr.slice(1,3)
+
+console.log(myn1)
+
+const myn2 =myArr.splice(1,3)
+console.log("c",myArr)
+console.log(myn2)
