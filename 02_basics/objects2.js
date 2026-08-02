a7beb87a148eb741b7d6bd35c9ca62a2862795b1
bharatswarajsahu <bharatswarajsahu@gmail.com>
2026-08-02 15:22:49 +0530

    objects2

---
+++ 02_basics/objects2.js
@@ -0,0 +1,62 @@
+/*const tinderuser =new Object();
+tinderuser.name="bharat";
+tinderuser.age=23;
+tinderuser.email="bharat@"
+console.log(tinderuser)//singleton object*/
+const tinderuser ={}
+tinderuser.name="bharat";
+tinderuser.age=23;
+tinderuser.email="bharat@"
+console.log(tinderuser)//non singleton object
+
+
+const regularuser ={
+ email:"bharat@g",
+ fullname :{
+    userfullname:{
+        firstname:"bharat",
+        lastname:"kumar",
+
+    }
+
+ }
+
+
+}
+//console.log(regularuser.fullname.userfullname.firstname)
+const obj1 ={
+    1:"a",
+    2:"b",
+}
+const obj2 ={
+    3:"a",
+    4:"b",
+
+}
+
+//const obj3 ={obj1,obj2}
+//const obj3 =Object.assign({},obj1,obj2)
+const obj3 ={...obj1,...obj2}
+//console.log(obj3)
+
+
+const users=[
+    {
+        id:1,
+        email:"bharat@56",
+    },
+   {
+        id:1,
+        email:"bharat@56",
+    },
+ {
+        id:1,
+        email:"bharat@56",
+    },
+]
+
+users[1].email
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+console.log(tinderuser.hasOwnProperty("age"))
